ab74cc5354c320da376a9781d2aa11dd0f53082b
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-20 21:46:18 +0530

    add l1

---
+++ assm/basic/ex1.asm
@@ -0,0 +1,15 @@
+global _start
+
+_start:
+    mov eax, 1
+    mov ebx, 42
+    sub ebx, 29
+    int 0x80
+
+; mov ebx,123 ; ebx = 123
+; mov eax,ebx ; eax = ebx
+; add ebx,ecx ; ebx += ecx
+; sub ebx,edx ; ebx -= edx
+; mul ebx     ; eax += ebx 
+; div edx     ; eax /= edx
+
